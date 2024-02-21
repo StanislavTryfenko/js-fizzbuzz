5f8b86d77adce34afee7da4e33ee6b711e4bba0d
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-21 16:37:40 +0100

    fixing table

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,31 @@ const ulElement = document.querySelector('.grid');
 
 for(let i = 1; i <= 49; i++){
     //console.log(i);
-    const markupListItem = `<li>${i}</li>`;
-    ulElement.insertAdjacentHTML('beforeend', markupListItem);
 
-    
+    if (i % 3 == 0){
+        const markupListItem = `<li>fizz</li>`;
+        ulElement.insertAdjacentHTML('beforeend', markupListItem)   
+    }
+    else if (i % 5 == 0){
+        const markupListItem = `<li>buzz</li>`;
+        ulElement.insertAdjacentHTML('beforeend', markupListItem)
+    }
+    else if ((i % 3 == 0) || (i % 5 ==0)){
+        const markupListItem = `<li>fizzbuzz</li>`;
+        ulElement.insertAdjacentHTML('beforeend', markupListItem);  
+    }
+    else{
+        const markupListItem = `<li>${i}</li>`;
+    ulElement.insertAdjacentHTML('beforeend', markupListItem) 
+    }
+
+
+
+
+
+
+
+    /**/;
+
+
 }
