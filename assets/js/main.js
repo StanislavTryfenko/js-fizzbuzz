08583fd8fc51c0d506bd9e679208a1912703f00e
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-21 17:18:37 +0100

    CONSEGNA

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,64 @@
-
-const ulElement = document.querySelector('.grid');
-
 for(let i = 1; i <= 49; i++){
-    //console.log(i);
 
-    if (i % 3 == 0){
-        const markupListItem = `<li>fizz</li>`;
-        ulElement.insertAdjacentHTML('beforeend', markupListItem)   
+    if (i % 3 == 0 && i % 5 ==0 ){
+        // Create an "li" node:
+        const li = document.createElement("li");
+
+        // Add a class to "li":
+        li.classList.add("fizzbuzz")
+
+        // Create a text node:
+        const liText = document.createTextNode("FizzBuzz");
+
+        // Append the text node to the "li" node:
+        li.appendChild(liText);
+
+        // Append the "li" node to the list:
+        document.getElementById("grid").appendChild(li);
     }
     else if (i % 5 == 0){
-        const markupListItem = `<li>buzz</li>`;
-        ulElement.insertAdjacentHTML('beforeend', markupListItem)
+        // Create an "li" node:
+        const li = document.createElement("li");
+
+        // Add a class to "li":
+        li.classList.add("buzz")
+
+        // Create a text node:
+        const liText = document.createTextNode("Buzz");
+
+        // Append the text node to the "li" node:
+        li.appendChild(liText);
+
+        // Append the "li" node to the list:
+        document.getElementById("grid").appendChild(li);
     }
-    else if ((i % 3 == 0) || (i % 5 ==0)){
-        const markupListItem = `<li>fizzbuzz</li>`;
-        ulElement.insertAdjacentHTML('beforeend', markupListItem);  
+    else if (i % 3 == 0){
+        // Create an "li" node:
+        const li = document.createElement("li");
+
+        // Add a class to "li":
+        li.classList.add("fizz")
+
+        // Create a text node:
+        const liText = document.createTextNode("Fizz");
+
+        // Append the text node to the "li" node:
+        li.appendChild(liText);
+
+        // Append the "li" node to the list:
+        document.getElementById("grid").appendChild(li);;   
     }
     else{
-        const markupListItem = `<li>${i}</li>`;
-    ulElement.insertAdjacentHTML('beforeend', markupListItem) 
+        // Create an "li" node:
+        const li = document.createElement("li");
+
+        // Create a text node:
+        const liText = document.createTextNode(i);
+
+        // Append the text node to the "li" node:
+        li.appendChild(liText);
+
+        // Append the "li" node to the list:
+        document.getElementById("grid").appendChild(li);;
     }
-
-
-
-
-
-
-
-    /**/;
-
-
 }
