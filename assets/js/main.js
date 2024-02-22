94be58c9c8347d479c63400f36adfce8913693f9
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-22 10:33:29 +0100

    optimizing code again

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 for(let i = 1; i <= 100; i++){
-    
+
     // Create an "li" node:
     const li = document.createElement("li");
 
@@ -8,39 +8,31 @@ for(let i = 1; i <= 100; i++){
         li.classList.add("fizzbuzz")
 
         // Create a text node:
-        const liText = document.createTextNode("FizzBuzz");
-
-        // Append the text node to the "li" node:
-        li.appendChild(liText);
+        var liText = document.createTextNode("FizzBuzz");
     }
     else if (i % 5 == 0){
         // Add a class to "li":
         li.classList.add("buzz")
 
         // Create a text node:
-        const liText = document.createTextNode("Buzz");
-
-        // Append the text node to the "li" node:
-        li.appendChild(liText);
+        var liText = document.createTextNode("Buzz");
     }
     else if (i % 3 == 0){
         // Add a class to "li":
         li.classList.add("fizz")
 
         // Create a text node:
-        const liText = document.createTextNode("Fizz");
-
-        // Append the text node to the "li" node:
-        li.appendChild(liText);
+        var liText = document.createTextNode("Fizz");
     }
     else{
         // Create a text node:
-        const liText = document.createTextNode(i);
+        var liText = document.createTextNode(i);
 
-        // Append the text node to the "li" node:
-        li.appendChild(liText);
     }
+    
+    // Append the text node to the "li" node:
+    li.appendChild(liText);
 
     // Append the "li" node to the list:
-        document.getElementById("grid").appendChild(li);
+    document.getElementById("grid").appendChild(li);
 }
